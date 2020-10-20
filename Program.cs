848d0cf89551fd913cfe6c6cc66a06fdafb6c139
kamalakars04 <kamalakars04@gmail.com>
2020-10-20 20:25:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MaximumTest
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TEST MAXIMUM program");
+        }
+    }
+}
